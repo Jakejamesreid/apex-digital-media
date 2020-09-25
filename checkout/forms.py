1d999e30885167e3979b306dd746b42044e56de9
Jake Reid <Jakejamesreid@gmail.com>
2020-09-25 17:20:51 +0000

    Add profile form, views and update template

--- checkout/forms.py
+++ checkout/forms.py
@@ -39,4 +39,3 @@ class OrderForm(forms.ModelForm):
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
-
