bc63aa128e1545b33e49f0f4e3beacfe325feee6
Jake Reid <Jakejamesreid@gmail.com>
2020-10-01 18:00:18 +0000

    Include client username in Stripe Payment intent and send client email

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+)
 from packages.models import Package
+from django.views.decorators.http import require_POST
 from django.conf import settings
 
 from .forms import OrderForm
@@ -8,7 +11,21 @@ from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
 
 import stripe
-import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        print(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 # package_id is default 0 for when payment form is submitted
