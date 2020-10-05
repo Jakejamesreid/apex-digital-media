f88617afe7a5db0d05acf0a6f8bbe44a73c87173
Jake Reid <Jakejamesreid@gmail.com>
2020-10-05 17:27:37 +0000

    App fully deployed on Heroku

--- apex_digital_media/settings.py
+++ apex_digital_media/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'dz#dwkf512&n-h^)o7rir8h1(om0+wsw+g-@dxw-ke*%n1rd*l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['apex-digital-media.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,12 +118,17 @@ WSGI_APPLICATION = 'apex_digital_media.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
