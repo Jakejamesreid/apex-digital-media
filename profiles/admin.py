ab7d82eb9a615546ee73d0aab546b9d15fb49327
Jake Reid <Jakejamesreid@gmail.com>
2020-10-24 12:28:23 +0000

    Implement functiopnality on request service buttona and send admin email with details

--- profiles/admin.py
+++ profiles/admin.py
@@ -4,14 +4,16 @@ from .models import UserProfile, ProfileLineItem
 
 class ProfileLineItemAdminInline(admin.TabularInline):
     model = ProfileLineItem
-    readonly_fields = ('package',)
+    readonly_fields = ('package', 'id')
     list_display = (
+        'id',
         'remaining_pages',
         'remaining_email_addresses',
         'remaining_seo_updates',
         'remaining_website_updates',
     )
     fields = (
+        'id',
         'package',
         'remaining_pages',
         'remaining_email_addresses',
--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
-
 from django.contrib.auth.decorators import login_required
-from .models import UserProfile, ProfileLineItem
-from .forms import UserProfileForm
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.conf import settings
+
 from services.forms import RequestServiceForm
+
+from .forms import UserProfileForm
+from .models import ProfileLineItem, UserProfile
 
 
 @login_required
@@ -41,15 +45,35 @@ def decrement_service(request, service_id, service_name):
 
         form = RequestServiceForm(request.POST)
         if form.is_valid():
-            service = ProfileLineItem.objects.filter(profile=profile, id=service_id).values()
+            service = ProfileLineItem.objects.filter(
+                profile=profile, id=service_id).values()
             if service[0][service_name] > 0:
                 data = {
                     service_name: service[0][service_name]-1
                 }
-                ProfileLineItem.objects.filter(profile=profile, id=service_id).update(**data)
+                ProfileLineItem.objects.filter(
+                    profile=profile, id=service_id).update(**data)
+
+                subject = render_to_string(
+                    'profiles/service_request_emails/email_subject.txt',
+                    {'request': service_name.replace('_', ' ')})
+                body = render_to_string(
+                    'profiles/service_request_emails/email_body.txt', {
+                        'user': profile.user,
+                        'service_name': service_name,
+                        'service_id': service_id,
+                        'description': request.POST['description']
+                    }
+                )
+                send_mail(
+                    subject,
+                    body,
+                    settings.DEFAULT_FROM_EMAIL,
+                    [settings.DEFAULT_FROM_EMAIL]
+                )
+
             else:
-                messages.error(request,
-                                f'You have used up this service.')
+                messages.error(request, 'You have used up this service.')
         else:
             messages.error(request, 'Form is invalid')
 
